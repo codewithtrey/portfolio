dc4ecce203ee95b1bc84961c15a64bf6d4b21e91
Trejon McGee <trejon.mcgee@gmail.com>
2023-12-22 11:44:16 -0600

    Update About component content

--- components/About.jsx
+++ components/About.jsx
@@ -13,22 +13,23 @@ const About = () => {
           </p>
           <h2 className="py-4">Who I Am</h2>
           <p className="py-2 text-gray-600">
-            I specialize in building front-end applications
-            that connect with API’s and other backend technologies. I’m
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, JavaScript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.
+            I specialize in building front-end applications that connect with
+            API’s and other back-end frameworks. I am passionate about learning
+            new technologies and understand there is more than one way to
+            accomplish a task. Though I am most proficient in building front-end
+            applications using HTML, CSS, JavaScript, and React, I am a quick
+            learner and can pick up new tech stacks as needed. I believe that
+            being a great developer is not using one specific language, but
+            choosing the best tool for the job.
           </p>
           <p className="py-2 text-gray-600">
             My technical expertise is complemented by my proficiency in
             efficiently solving intricate problems through a blend of creative
-            and analytical thinking. Furthermore, I have earned certifications as an
-            AWS Certified Solutions Architect Associate and an AWS Certified Cloud
-            Practitioner, underlining my commitment to continuous learning and
-            staying updated with the latest industry standards.
+            and analytical thinking. Furthermore, I have earned certifications
+            as an AWS Certified Solutions Architect Associate and an AWS
+            Certified Cloud Practitioner, underlining my commitment to
+            continuous learning and staying updated with the latest industry
+            standards.
           </p>
           <Link href="/#projects">
             <p className="py-2 text-gray-600 underline cursor-pointer">
